099433b7f272a40c0b827ae14e31dae30588c944
GustavoAdriano1E <00001121050335sp@al.educacao.sp.gov.br>
2025-03-13 08:30:56 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showNumber(123)
+basic.showString("GO!")
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showString("I ")
+    basic.showIcon(IconNames.Heart)
+    basic.showString("YOU")
+})
